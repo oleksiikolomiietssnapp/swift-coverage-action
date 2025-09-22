9712843b1e98dfe2e3980ac1c10cd70725110c36
Oleksii Kolomiiets <oleksii@snapp.team>
2025-09-22 18:11:12 +0200

    simplify sample fonts use case

--- sample/Sources/UtilsLibrary/FontUtils.swift
+++ sample/Sources/UtilsLibrary/FontUtils.swift
@@ -8,7 +8,7 @@ import AppKit
 public typealias PlatformFont = NSFont
 #endif
 
-/// Cross-platform font utilities for iOS and macOS
+/// Simple cross-platform font utilities for iOS and macOS
 public struct FontUtils {
 
     public init() {}
@@ -24,57 +24,9 @@ public struct FontUtils {
         #endif
     }
 
-    /// Creates a bold system font with the specified size
-    public func boldSystemFont(size: CGFloat) -> PlatformFont? {
-        #if canImport(UIKit)
-        return UIFont.boldSystemFont(ofSize: size)
-        #elseif canImport(AppKit)
-        return NSFont.boldSystemFont(ofSize: size)
-        #else
-        return nil
-        #endif
-    }
-
     /// Gets the font size from a platform font
     public func fontSize(from font: PlatformFont) -> CGFloat {
         return font.pointSize
-    }
-
-    /// Checks if a font is bold
-    public func isBold(_ font: PlatformFont) -> Bool {
-        #if canImport(UIKit)
-        let traits = font.fontDescriptor.symbolicTraits
-        return traits.contains(.traitBold)
-        #elseif canImport(AppKit)
-        let traits = font.fontDescriptor.symbolicTraits
-        return traits.contains(.bold)
-        #else
-        return false
-        #endif
-    }
-
-    /// Gets the font family name
-    public func familyName(from font: PlatformFont) -> String? {
-        return font.familyName
-    }
-
-    /// Compares two fonts for equality based on family name and size
-    public func areEqual(_ font1: PlatformFont, _ font2: PlatformFont) -> Bool {
-        guard let family1 = font1.familyName, let family2 = font2.familyName else {
-            return false
-        }
-        return family1 == family2 && font1.pointSize == font2.pointSize
-    }
-
-    /// Scales a font to a new size
-    public func scale(_ font: PlatformFont, to newSize: CGFloat) -> PlatformFont? {
-        #if canImport(UIKit)
-        return font.withSize(newSize)
-        #elseif canImport(AppKit)
-        return NSFont(descriptor: font.fontDescriptor, size: newSize)
-        #else
-        return nil
-        #endif
     }
 
     /// Validates if a font size is within reasonable bounds
--- sample/Tests/UtilsLibraryTests/FontUtilsTests.swift
+++ sample/Tests/UtilsLibraryTests/FontUtilsTests.swift
@@ -14,59 +14,14 @@ struct FontUtilsTests {
         }
     }
 
-    @Test func testBoldSystemFont() {
-        let utils = FontUtils()
-        let font = utils.boldSystemFont(size: 18.0)
-
-        #expect(font != nil)
-        if let font = font {
-            #expect(utils.fontSize(from: font) == 18.0)
-            #expect(utils.isBold(font) == true)
-        }
-    }
-
     @Test func testFontSizeExtraction() {
         let utils = FontUtils()
-        let font = utils.systemFont(size: 24.0)
+        let font = utils.systemFont(size: 18.0)
 
         #expect(font != nil)
         if let font = font {
-            #expect(utils.fontSize(from: font) == 24.0)
-        }
-    }
-
-    @Test func testFontComparison() {
-        let utils = FontUtils()
-        let font1 = utils.systemFont(size: 16.0)
-        let font2 = utils.systemFont(size: 16.0)
-        let font3 = utils.systemFont(size: 20.0)
-
-        #expect(font1 != nil)
-        #expect(font2 != nil)
-        #expect(font3 != nil)
-
-        if let font1 = font1, let font2 = font2, let font3 = font3 {
-            #expect(utils.areEqual(font1, font2) == true)
-            #expect(utils.areEqual(font1, font3) == false)
-        }
-    }
-
-    @Test func testFontScaling() {
-        let utils = FontUtils()
-        let originalFont = utils.systemFont(size: 16.0)
-
-        #expect(originalFont != nil)
-        if let originalFont = originalFont {
-            let scaledFont = utils.scale(originalFont, to: 20.0)
-            #expect(scaledFont != nil)
-
-            if let scaledFont = scaledFont {
-                #expect(utils.fontSize(from: scaledFont) == 20.0)
-                // Compare family names handling optionals
-                let originalFamily = utils.familyName(from: originalFont)
-                let scaledFamily = utils.familyName(from: scaledFont)
-                #expect(originalFamily == scaledFamily)
-            }
+            let actualSize = utils.fontSize(from: font)
+            #expect(actualSize == 18.0)
         }
     }
 
@@ -86,18 +41,6 @@ struct FontUtilsTests {
         #expect(utils.defaultSize(for: .body) == 17.0)
         #expect(utils.defaultSize(for: .headline) == 22.0)
         #expect(utils.defaultSize(for: .title) == 28.0)
-    }
-
-    @Test func testFamilyName() throws {
-        let utils = FontUtils()
-        let font = utils.systemFont(size: 16.0)
-
-        #expect(font != nil)
-        if let font = font {
-            let familyName = try #require(utils.familyName(from: font))
-            #expect(!familyName.isEmpty)
-            // System font family name varies by platform but should always be non-empty
-        }
     }
 
     // Note: Not testing all edge cases to maintain partial coverage
